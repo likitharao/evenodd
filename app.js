10047a3f2021663b29b6930ed84bcbce6f55d24c
likitharao <likitha904@gmail.com>
2024-12-10 18:30:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+ 
+
+ //print even numbers
+//  for(i=1;i<=10;i++)
+//     {
+//         if(i%2==0)
+//         console.log(i);
+//     }
+
+let i=1;
+while(i<=10)
+    {  
+        i++;
+        if(i%2==0)
+        console.log(i);
+    }
+    //print odd numbers
+// for(j=0;j<=10;j++)
+//     {
+//         if(j%2==!0)
+//         console.log(j);
+//     }
